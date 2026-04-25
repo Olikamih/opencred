b85bbced0d1db6e5fb3f3e12ef1c5b3c324742cf
Kamila Oliveira <olihcdart@gmail.com>
2026-04-25 16:36:29 -0300

    fix: database railway config final

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,21 +13,19 @@ import { User } from './users/user.entity';
       isGlobal: true,
     }),
 
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-        type: 'postgres',
-        host: config.get<string>('DB_HOST'),
-        port: Number(config.get<string>('DB_PORT')),
-        username: config.get<string>('DB_USER'),
-        password: config.get<string>('DB_PASSWORD'),
-        database: config.get<string>('DB_NAME'),
-        schema: 'bank_data',
-        entities: [User, Transaction],
-        synchronize: true,
-      }),
-    }),
+   TypeOrmModule.forRootAsync({
+  imports: [ConfigModule],
+  inject: [ConfigService],
+  useFactory: (config: ConfigService) => ({
+    type: 'postgres',
+    url: config.get<string>('DATABASE_URL'),
+    autoLoadEntities: true,
+    synchronize: true,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  }),
+}),
 
     UsersModule,
     AuthModule,
