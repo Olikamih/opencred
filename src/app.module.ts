8d0188c0afd6b35c590e19f837f93775847b60c0
Kamila Oliveira <olihcdart@gmail.com>
2026-04-25 16:06:10 -0300

    fix: build + config dist + database schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,37 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { TransactionsModule } from './transactions/transactions.module';
-// import { AuditModule } from './audit/audit.module'; // 👈 COMENTADO
 import { Transaction } from './transactions/transaction.entity';
 import { User } from './users/user.entity';
-// import { Audit } from './audit/audit.entity'; // 👈 COMENTADO
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'database.sqlite',
-      entities: [User, Transaction], // 👈 REMOVIDO AUDIT DAQUI
-      synchronize: true, 
+    ConfigModule.forRoot({
+      isGlobal: true,
     }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get<string>('DB_HOST'),
+        port: Number(config.get<string>('DB_PORT')),
+        username: config.get<string>('DB_USER'),
+        password: config.get<string>('DB_PASSWORD'),
+        database: config.get<string>('DB_NAME'),
+        schema: 'bank_data',
+        entities: [User, Transaction],
+        synchronize: true,
+      }),
+    }),
+
     UsersModule,
     AuthModule,
     TransactionsModule,
-    // AuditModule, // 👈 COMENTADO
   ],
 })
 export class AppModule {}
